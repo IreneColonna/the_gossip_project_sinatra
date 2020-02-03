ff03dc278e7172537f8a2765eb0dbc5ac4a448ce
Irène Colonna <B00684776@essec.edu>
2020-02-03 15:54:42 +0100

    second ajout - jusqu'au 2.6

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,24 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
 
   get '/' do
-		erb :index
+		erb :index, locals: {gossips: Gossip.all}
 	end
+
+	get '/gossips/new/' do
+		erb :new_gossip
+	end
+
+	post '/gossips/new/' do
+		Gossip.new(params["gossip_author"], params["gossip_content"]).save
+		redirect '/'
+	end
+
+#	get '/hello/:name' do
+	  # matches "GET /hello/foo" and "GET /hello/bar"
+	  # params['name'] is 'foo' or 'bar'
+#		 "Hello #{params['name']}!"
+#	end
 
 end
---
+++ lib/gossip.rb
@@ -0,0 +1,30 @@
+require 'pry'
+require 'csv'
+
+class Gossip
+	attr_accessor :author, :content
+
+
+	def initialize(author, content)
+		@author = author
+		@content = content
+	end 
+
+	def save
+		CSV.open("./db/gossip.csv", "ab") do |csv|
+    csv << [@author, @content]
+		end
+	end
+
+	def self.all
+		all_gossips = []
+		CSV.foreach("./db/gossip.csv") do |csv_line|
+			gossip_temp = Gossip.new(csv_line[0], csv_line[1])
+			all_gossips << gossip_temp
+		end
+		all_gossips
+	end	
+
+end
+
+
